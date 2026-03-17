78f8e0e2d8008be7970c2b913e55be09b020aa2b
GlebGrigorjev <glebgrigorjev@gmail.com>
2026-03-17 18:55:20 +0200

    Backend prep

---
+++ backend/api/api/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ backend/api/api/Models/Stock.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+
+        [Column(TypeName = "numeric(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "numeric(18,2)")]
+        public decimal LastDividend { get; set; }
+
+        public List<Comment> Comment { get; set; } = [];
+    }
+}
